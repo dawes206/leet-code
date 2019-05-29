4230e441e436106a444ec8837913f44a49530c1b
Silas Walker <sw099y@att.com>
2019-05-29 14:03:09 -0700

    Solved plus_one problem

---
+++ leet_code_problem_num.py
@@ -0,0 +1,46 @@
+# input: [1,2,3]
+# output: [1,2,4]
+
+
+class Solution():
+    def plusOne(self,nums):
+        for i in range(1, len(nums)+1):
+            print(i)
+            if nums[-i]+1 != 10:
+                nums[-i] += 1
+                return nums
+            elif i == len(nums):
+                nums[-i] = 0
+                return [1] + nums
+            else:
+                nums[-i] = 0
+        return nums
+
+test = Solution()
+print(test.plusOne([9,9,9]))
+
+
+
+
+# if last number == 10
+#     set it equal to zero
+#     move to next value
+#     check if it's == 10
+#     if so, move to next
+#     if not, just subtract 1
+# else just add 1
+#
+#
+# for every number, starting from back
+#     if number + 1 != 10
+#         number + 1
+#         return
+#     else
+#         set number = 0
+#         move to next number
+#         if number +1 !=10
+#             number + 1
+#             return
+#         else
+#             set number = 0
+#             move to next number...
