e8d4e5ed6d8aa10df9e540e38690447bb092ab3e
Silas Walker <sw099y@att.com>
2019-05-29 12:14:31 -0700

    first commit

---
+++ binary_tree_practice.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self,key):
+        self.left = None
+        self.right = None
+        self.value = key
+
+
+root = Node(1)
+
+print(type(root))
---
+++ two_sums_practice.py
@@ -0,0 +1,51 @@
+import time
+class Solution:
+    # def twoSum(self, nums: list, target: int):
+    #     for i in nums:
+    #         for x in nums:
+    #             if i+x == target:
+    #                 return [nums.index(i), nums.index(x)]
+
+    def twoSum2(self, nums: list, target: int):
+        x=list(nums)
+        for i in nums:
+            x.remove(i)
+            if target-i in x:
+                return [nums.index(i), nums.index(target-i)]
+    # def twoSum3(self, nums: list, target: int):
+    #     x = []
+    #     for i in nums:
+    #         x.append(i)
+    #         if target - i !=i and target-i in nums:
+    #             if target-i in x:
+    #                 return [nums.index(i), x.index(target-i)]
+    #             else:
+    #                 return [nums.index(i), nums.index(target-i)]
+
+# def create_dataset(size):
+#     dataset = list(range(1,size))
+#     for i in dataset[1:int(size/2)-2]:
+#         dataset.remove(size-i)
+#     return dataset
+
+
+
+# test = Solution()
+# greeting = test.twoSum([1,2],2)
+#
+# dataset = create_dataset(10000)
+#
+
+answer = Solution()
+# time1 = time.perf_counter()
+# print(answer.twoSum(dataset,10000))
+# time2 = time.perf_counter()
+print(answer.twoSum2([3,3],6))
+# time3 = time.perf_counter()
+# print(answer.twoSum3(dataset,10000))
+# time4 = time.process_time()
+#[1, 2, 7, 8, 11, 15],15)
+
+# print('time for first', time2-time1)
+# print('time for 2nd', time3-time2)
+# print('time for 3rd', time4-time3)
