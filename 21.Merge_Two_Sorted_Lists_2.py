bed1a0b476b7c51f75969d5b1771fae563658e0a
Silas Walker <sw099y@att.com>
2019-06-07 08:00:44 -0700

    Worked on merge_two_sorted_lists again, and understood itgit add .

---
+++ 21.Merge_Two_Sorted_Lists_2.py
@@ -0,0 +1,43 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        left = l1
+        right = l2
+        root = ListNode(None)
+        node = root
+        while left and right:
+            if left.val<=right.val:
+                node.next = left
+                left = left.next
+            else:
+                node.next = right
+                right = right.next
+            node = node.next
+        if left:
+            print('left is not none')
+            node.next = left
+        elif right:
+            print('right is not none')
+            node.next = right
+        return root
+
+
+L = ListNode(1)
+L.next = ListNode(2)
+L.next.next =ListNode(4)
+R = ListNode(1)
+R.next = ListNode(3)
+R.next.next = ListNode(4)
+
+
+test = Solution()
+ans = test.mergeTwoLists(L,R)
+print(ans)
+mover = ans
+while mover:
+    print('Node: ', mover.val)
+    mover = mover.next
